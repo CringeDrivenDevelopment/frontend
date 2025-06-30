24d2fa70395f5356842ee348ccfb14eafb414e0a
Ustaz1505 <nikma-1@Outlook.com>
2025-06-30 16:07:01 +0300

    fix: fix devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  pages: true,
+  ssr: false,
+  modules: ['@nuxtjs/tailwindcss']
 })
