7bc24028bfe33e9969ffc5b0b209ff44de9f7d66
Ustaz1505 <nikma-1@Outlook.com>
2025-07-01 10:24:16 +0300

    feat: restructure app layout and add FooterNavBar component

--- app.vue
+++ app.vue
@@ -1,3 +1,8 @@
 <template>
-  <NuxtPage />
+  <div class="min-h-screen flex flex-col">
+    <NuxtPage />
+    <div class="mt-auto">
+      <FooterNavBar />
+    </div>
+  </div>
 </template>
---
+++ components/FooterNavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="h-20 bg-gray-800 text-white flex items-center justify-center">
+    <nav class="w-full flex items-center justify-around">
+      <NuxtLink to="">
+        <LucideListCheck class="size-8 text-white" />
+      </NuxtLink>
+      <NuxtLink to="/">
+        <LucideHome class="size-10 text-white" />
+      </NuxtLink>
+      <NuxtLink to="">
+        <LucideListMusic class="size-8 text-white" />
+      </NuxtLink>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { LucideHome, LucideListCheck, LucideListMusic } from '#components';
+
+
+</script>
+
+<style>
+
+</style>
