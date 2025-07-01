3c3946a42f7eca6bd668acb49e6a3181a2310b3e
Ustaz1505 <nikma-1@Outlook.com>
2025-07-01 10:24:40 +0300

    fix: update navigation links in FooterNavBar component

--- components/FooterNavBar.vue
+++ components/FooterNavBar.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="h-20 bg-gray-800 text-white flex items-center justify-center">
     <nav class="w-full flex items-center justify-around">
-      <NuxtLink to="">
+      <NuxtLink to="/playlist">
         <LucideListCheck class="size-8 text-white" />
       </NuxtLink>
       <NuxtLink to="/">
         <LucideHome class="size-10 text-white" />
       </NuxtLink>
-      <NuxtLink to="">
+      <NuxtLink to="/catalog">
         <LucideListMusic class="size-8 text-white" />
       </NuxtLink>
     </nav>
