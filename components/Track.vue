3ec491cca16308c1556b7a9b5d5a5b9f3324029c
Ustaz1505 <nikma-1@Outlook.com>
2025-07-01 15:51:10 +0300

    feat: add Track component and playlist page to display tracks

---
+++ components/Track.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ track.title }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { components } from '#open-fetch-schemas/api';
+import { defineProps } from 'vue'
+
+defineProps<{
+  track: components['schemas']['Track']
+}>()
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/playlist.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Track v-for="track in tracks" :key="track.id" :track="track" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const tracks = [
+  {
+    "authors": "string",
+    "explicit": true,
+    "id": "string",
+    "length": 0,
+    "thumbnail": "string",
+    "title": "string"
+  },
+  {
+    "authors": "string",
+    "explicit": true,
+    "id": "string",
+    "length": 0,
+    "thumbnail": "string",
+    "title": "string"
+  },
+  {
+    "authors": "string",
+    "explicit": true,
+    "id": "string",
+    "length": 0,
+    "thumbnail": "string",
+    "title": "string"
+  }
+]
+
+</script>
+
+<style>
+
+</style>
