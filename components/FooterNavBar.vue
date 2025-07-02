37f09f93d7e034dfa79fda449c472c6bef7cd90f
rediska <sherstnev.denis.v@gmail.com>
2025-07-02 16:04:50 +0300

    feat: animations

--- components/FooterNavBar.vue
+++ components/FooterNavBar.vue
@@ -2,13 +2,13 @@
   <div class="h-16 bg-slate-800 text-white flex items-center justify-center">
     <nav class="w-full flex items-center justify-around h-full">
       <NuxtLink to="/playlist" class="w-1/3 flex justify-center items-center h-full">
-        <LucideListCheck class="size-8" :class="{'text-indigo-400': route.name === 'playlist'}" />
+        <LucideListCheck class="size-8 transition-all duration-200" :class="{'text-indigo-400 drop-shadow-indigo-400 drop-shadow-2xl': route.name === 'playlist'}" />
       </NuxtLink>
       <NuxtLink to="/" class="w-1/3 flex justify-center items-center h-full">
-        <LucideHome class="size-10" :class="{'text-indigo-400': route.name === 'index'}" />
+        <LucideHome class="size-10 transition-all duration-200" :class="{'text-indigo-400 drop-shadow-indigo-400 drop-shadow-2xl': route.name === 'index'}" />
       </NuxtLink>
       <NuxtLink to="/catalog" class="w-1/3 flex justify-center items-center h-full">
-        <LucideListMusic class="size-8" :class="{'text-indigo-400': route.name === 'catalog'}" />
+        <LucideListMusic class="size-8 transition-all duration-200" :class="{'text-indigo-400 drop-shadow-indigo-400 drop-shadow-2xl': route.name === 'catalog'}" />
       </NuxtLink>
     </nav>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -23,14 +23,14 @@ const miniApp = useMiniApp();
     <div class="flex flex-wrap gap-4 w-11/12">
       <NuxtLink
         to="/catalog"
-        class="w-full rounded-xl bg-indigo-500 p-4 hover:shadow-indigo-500 hover:shadow-2xl transition-shadow text-center text-lg flex items-center gap-5"
+        class="w-full rounded-xl bg-indigo-500 p-4 hover:shadow-indigo-500 hover:shadow-2xl transition-shadow text-center text-lg flex items-center gap-4"
       >
         <LucideMusic :size="20" />
         <span>Добавить трек в плейлист</span>
       </NuxtLink>
       <NuxtLink
         to="/playlist"
-        class="w-full rounded-xl bg-indigo-500 p-4 hover:shadow-indigo-500 hover:shadow-2xl transition-shadow text-center text-lg flex items-center gap-5"
+        class="w-full rounded-xl bg-indigo-500 p-4 hover:shadow-indigo-500 hover:shadow-2xl transition-shadow text-center text-lg flex items-center gap-4"
       >
         <LucideDisc3 :size="20" />
         <span>Посмотреть плейлисты</span>
