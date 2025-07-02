485fe6a6e81050c47c7fd8cab44e955945bd70b0
Ustaz1505 <nikma-1@Outlook.com>
2025-07-02 10:17:36 +0300

    fix: update Track component styles and improve title display logic

--- components/Track.vue
+++ components/Track.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full flex justify-between items-center rounded-lg cursor-pointer transition-colors hover:bg-indigo-300/10 p-2 group">
     <div class="flex items-center gap-3">
-      <div class="rounded-lg w-13 h-13 relative">
+      <div class="rounded-lg size-13 aspect-square relative">
         <div class="w-full h-full rounded-lg bg-indigo-500/40 absolute top-0 z-20 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity">
           <LucideCirclePlay :size="27" class="hover:text-indigo-100 transition-colors duration-200" />
         </div>
@@ -9,7 +9,7 @@
       </div>
       <div class="flex flex-wrap content-between">
         <div class="w-full flex gap-2 items-center">
-          <span class="text-lg font-medium">{{ track.title.length > MAX_SYMBOLS ? track.title.substring(0, MAX_SYMBOLS) + "..." : track.title }}</span>
+          <span class="text-lg font-medium whitespace-nowrap truncate w-35">{{ track.title }}</span>
           <LucideCircleAlert :size="15" v-if="track.explicit" />
         </div>
         <span class="w-full text-md text-neutral-400">{{ track.authors }}</span>
