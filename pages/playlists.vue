477a1788cca16f0a450dcadb5406899df32c36f4
Ustaz1505 <nikma-1@Outlook.com>
2025-07-03 12:34:31 +0300

    fix: add message for empty playlists

--- pages/playlists.vue
+++ pages/playlists.vue
@@ -7,6 +7,11 @@
         :key="playlist.id"
         :playlist="playlist"
       />
+      <div
+        v-if="!playlists || playlists.length === 0"
+        class="w-full text-center text-gray-500">
+        У вас пока нет плейлистов. Создайте свой первый плейлист!
+      </div>
     </div>
   </div>
 </template>
